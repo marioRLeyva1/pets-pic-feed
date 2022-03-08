961724c37979e764335a058930f44112dea4392a
marioRLeyva1 <mario_980615@hotmail.com>
2022-03-07 23:09:25 -0600

    v1.0.2

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -7,8 +7,16 @@ export const About = () => {
         <li>Create a context to reuse the Modal, in the context must be included title, msg, type (warn, error, success)</li>
         <li>Add a broken heart when a favorited is removed</li>
         <li>Create Not Found page</li>
-        <li>Firebase</li>
       </ul>
+
+      <h2>Release Notes</h2>
+      <section>
+        <h5>v1.0.2</h5>
+        <ul>
+          <li>Heart added when you add to favorites some image.</li>
+          <li>Bug fixes</li>
+        </ul>
+      </section>
     </div>
   );
 };
