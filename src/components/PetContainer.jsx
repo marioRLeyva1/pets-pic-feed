97527e4f564708340ac081514afbfe82e41d0653
marioRLeyva1 <mario_980615@hotmail.com>
2022-03-07 23:04:23 -0600

    heart in right position

--- src/components/PetContainer.jsx
+++ src/components/PetContainer.jsx
@@ -31,7 +31,7 @@ const PetContainer = (props) => {
     <section className="flex justify-center">
       <div className="relative flex justify-center shadow-2xl rounded bg-slate-400 border-2 border-slate-300 w-full h-96 md:hover:scale-105 transition">
         <img className={`relative top-0 left-0 sm:w-full sm:h-full object-contain ${added == true && 'opacity-50 transition'}`} src={props.src} onDoubleClick={add(props.src)}></img>
-        <img className={`absolute top-0 left-0 object-contain ${added == false && 'hidden'}`} src="/images/white-heart.png" alt="liked" />
+        <img className={`absolute md:fixed top-0 left-0 object-contain md:w-full h-full md:scale-75 ${added == false && 'hidden'}`} src="/images/white-heart.png" alt="liked" />
       </div>
     </section>
   );
